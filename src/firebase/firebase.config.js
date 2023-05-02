89cbe52e26e51df8101fb0bfdf5b4fb8955d26cf
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-02 11:24:14 +0600

    firebase configer done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_apx5ipZicEgeOou4ZjBaAmuh7RHrCp4",
+  authDomain: "food-hub-4ef88.firebaseapp.com",
+  projectId: "food-hub-4ef88",
+  storageBucket: "food-hub-4ef88.appspot.com",
+  messagingSenderId: "371156312701",
+  appId: "1:371156312701:web:d08a468efb6c578117c714"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
